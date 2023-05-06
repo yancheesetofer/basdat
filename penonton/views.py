9419e8b1a631549f64f6f34ba3d11182944a2bb1
valencius.apriady <v.apriady@gmail.com>
2023-05-06 18:17:55 +0700

    done pilihStadiumPage

--- penonton/views.py
+++ penonton/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 
 # Create your views here.
 
 def index(request):
     return render(request, "awal.html")
+
+def listWaktuStadium(request):
+    if (request.method) == 'POST':
+        stadium = request.get("stadium")
+        tanggal = request.get("tanggal")
+        return render(request, 'listWaktuStadium.html', {stadium, tanggal})
