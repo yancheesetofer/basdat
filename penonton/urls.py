d127a206eafc9cf460af0be772a417e74199c00c
valencius.apriady <v.apriady@gmail.com>
2023-05-06 19:31:59 +0700

    added page of listPertandingan and listWaktu

--- penonton/urls.py
+++ penonton/urls.py
@@ -4,5 +4,7 @@ from django.urls import path
 app_name = "penonton"
 
 urlpatterns = [
-    path('', index, name="index")
+    path("", index, name="index"),
+    path("waktu/", listWaktuStadium, name="waktu"),
+    path("list/", listPertandingan, name="list")
 ]
--- penonton/views.py
+++ penonton/views.py
@@ -4,10 +4,10 @@ from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 # Create your views here.
 
 def index(request):
-    return render(request, "awal.html")
+    return render(request, "pilihStadium.html")
 
 def listWaktuStadium(request):
-    if (request.method) == 'POST':
-        stadium = request.get("stadium")
-        tanggal = request.get("tanggal")
-        return render(request, 'listWaktuStadium.html', {stadium, tanggal})
+    return render(request, 'listWaktuStadium.html')
+
+def listPertandingan(request):
+    return render(request, 'listPertandingan.html')
