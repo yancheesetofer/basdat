a27900678c45b64eef5713cf820a4afcdff82f3b
Marthin Lubis <marthin.firman@ui.ac.id>
2023-05-25 20:34:29 +0700

    CRUD panitia

--- panitia/views.py
+++ panitia/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.conf import settings
 from django.db import *
-
+from django.http import HttpResponse
 # Create your views here.
 
 
@@ -48,10 +48,54 @@ def list_peristiwa(request):
 
 
 def buat_pertandingan(request):
-    return render(request, "buat_pertandingan.html")
+    daftar_stadium={}
+    try:
+        cursor = connection.cursor()
+        query = """
+                SELECT nama
+FROM stadium;
+            """
+        cursor.execute(query)
+        daftar_stadium = cursor.fetchall()
+        
+    finally:
+        # closing database connection.
+        if connection:
+            connection.commit()
+            cursor.close()
+            connection.close()
+            print("PostgreSQL connection is closed")
+    
+    context = {
+            'ds':daftar_stadium}
+    return render(request, "buat_pertandingan.html",context)
 
 def list_pertandingan(request):
-    return render(request, "list_pertandingan.html")
+    daftar_pertandingan={}
+    try:
+        cursor = connection.cursor()
+        query = """
+                SELECT string_agg(DISTINCT nama_tim, ' VS ' )
+FROM tim_pertandingan
+GROUP BY id_pertandingan;
+            """
+        cursor.execute(query)
+        daftar_pertandingan = cursor.fetchall()
+        
+        
+    finally:
+        # closing database connection.
+        if connection:
+            connection.commit()
+            cursor.close()
+            connection.close()
+            print("PostgreSQL connection is closed")
+    
+    list_tim=zip(daftar_pertandingan[0:4],daftar_pertandingan[4:8])
+    context = {
+            'lt':list_tim}
+    
+    return render(request, "list_pertandingan.html",context)
 
 def mulai_rapat(request):
     return render(request, "mulai_rapat.html")
@@ -60,7 +104,40 @@ def nota_rapat(request):
     return render(request, "nota_rapat.html")
 
 def submit_pertandingan(request):
-    return render(request, "submit_pertandingan.html")
+    daftar_wasit={}
+    daftar_tim={}
+    try:
+        cursor = connection.cursor()
+        query = """
+                Select distinct W.id_wasit
+                From wasit W
+                WHERE W.id_wasit NOT IN 
+                (SELECT WB.id_wasit From wasit_bertugas WB);
+            """
+        cursor.execute(query)
+        daftar_wasit = cursor.fetchall()
+        
+        cursor = connection.cursor()
+        query = """
+                Select nama_tim
+                From tim;
+            """
+        cursor.execute(query)
+        daftar_tim = cursor.fetchall()
+        
+    finally:
+        # closing database connection.
+        if connection:
+            connection.commit()
+            cursor.close()
+            connection.close()
+            print("PostgreSQL connection is closed")
+
+    context = {
+            'dw':daftar_wasit,
+            'dt1':daftar_tim[0:15],
+            'dt2':daftar_tim[16:]}
+    return render(request, "submit_pertandingan.html",context)
 
 def schedule_list(request):
     return render(request, "schedule_list.html")
